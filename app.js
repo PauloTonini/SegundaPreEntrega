d9d763eabbab35e2fcd825f69b7dd26a12e039f1
PauloTonini <125523290+PauloTonini@users.noreply.github.com>
2023-05-03 23:23:14 -0300

    Segunda preentrega

---
+++ app.js
@@ -0,0 +1,8 @@
+const productos = [];
+
+productos.push(new Producto("azucar","fronterita",45.50,"az"))
+productos.push(new Producto ("cafe","la virginia",78.90,"cf"))
+pruductos.push(new Producto("agua","villavicencio",24.30, "ag"))
+productos.push(new Producto("insecticida","raid",12.30,"in"))
+
+
---
+++ producto.js
@@ -0,0 +1,15 @@
+class Producto {
+    nombre;
+    marca;
+    precio;
+    codigo;
+
+    constructor(nombre,marca,precio,codigo)
+    {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
+}
