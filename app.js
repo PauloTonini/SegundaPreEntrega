6b54b0032ace7e77927c19372779fa3a60203929
PauloTonini <125523290+PauloTonini@users.noreply.github.com>
2023-05-15 14:24:50 -0300

    segunda pre entrega

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const carta = {
+    platos: [
+        {nombre: "Hamburguesa", precio: 1200},
+        {nombre: "Pizza", precio: 1500},
+        {nombre: "Ensalada", precio: 750}
+    ],
+    bebidas: [
+        {nombre: "Gaseosa", precio: 370 },
+        {nombre: "Agua" , precio: 200},
+        {nombre: "Cerveza", precio: 400}
+    ],
+    postres: [
+        {nombre: "Helado", precio: 240},
+        {nombre: "Chocotorta", precio: 300},
+        {nombre: "LemonPie", precio: 250}
+    ] 
+}
 
 const pedido = new Pedido();
 
